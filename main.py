5138743a14542bbfb98eaedac6ef729942759704
Noelis28 <lyamber64@gmail.com>
2025-04-28 19:21:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,53 @@
-import random
-characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+import discord
+from discord.ext import commands
+from bot_logic import *
 
-length = int(input("¿De cuántos caracteres quieres tu contraseña?: "))
+intents = discord.Intents.default()
+intents.message_content = True
 
-password = ""
+bot = commands.Bot(command_prefix= "$", intents=intents)
 
-for i in range(length):
-    password = password + random.choice(characters)
+@bot.event
+async def on_ready():
+    print(f'Tu bot {bot.user} esta en linea!')
+    
+@bot.command()
+async def saludar(ctx):
+    await ctx.send("¡Hola! Soy tu bot, ¿en qué puedo ayudarte hoy?")
+    
+@bot.command()      
+async def pregunta(ctx,*, mensaje:str):
+    response = chatbot_response(mensaje)
+    await ctx.send(response)
 
-print(f'tu contraseña es{password}')
+@bot.command()
+async def emoji(ctx):
+    await ctx.send(gen_emodji())
+    
+@bot.command()
+async def coin(ctx):
+    await ctx.send(flip_coin())
+
+@bot.command()
+async def password(ctx):
+    contraseña = gen_pass(10) 
+    await ctx.send(f'Tu contraseña generada es: {contraseña}')
+
+
+@bot.command()
+async def hora(ctx):
+    await ctx.send(f'La hora actual es: {get_current_time()}')
+
+@bot.command()
+async def randomnum(ctx, min_num: int, max_num: int):
+    await ctx.send(f'Número aleatorio entre {min_num} y {max_num}: {random_number(min_num, max_num)}')
+
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a + b)
+    
+@bot.command()
+async def joined(ctx, *, member: discord.Member):
+    await ctx.send(f'{member} joined on {member.joined_at}')
+
+bot.run ("")
